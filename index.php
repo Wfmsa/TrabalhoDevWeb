7ac57ab2c6863a03b213100a20f36b8c0a11253a
Wallace Luigge Pinhati Silva - 11816229 <11816229@animaedu.intranet>
2019-10-04 20:29:48 -0300

    Primeiro Arquivo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+define("IP","127.0.0.1");
+echo IP;
+echo "<br>";
+$numero=1;
+echo "hello World";
+echo "<br>";
+echo $numero;
+echo "<br>";
+$num1=100;
+$num2=400;
+echo("A soma das Variaveis 1 e 2 é:".($num1+$num2));
